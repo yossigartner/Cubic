fe6a2ec6584851712488693014831004c7350d9f
Yossi Gartner <yossi.gartner@ecoplant.co>
2021-06-17 21:13:09 +0100

    add save button

--- app/src/main/java/com/example/cubic/SaveMeasurement.kt
+++ app/src/main/java/com/example/cubic/SaveMeasurement.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class SaveMeasurement : Activity() {
     private var TAG = "Save Measurement Activity"
@@ -13,6 +15,8 @@ class SaveMeasurement : Activity() {
         setContentView(R.layout.activity_save_measurement)
         val measurementResult = intent.getFloatExtra(getString(R.string.EXTRA_MEASUREMENT_RESULT),0f);
         Log.d(TAG,"$measurementResult")
+        var measurementDimensionTextView = findViewById<TextView>(R.id.measurementDimesionInput)
+        measurementDimensionTextView.text ="$measurementResult SQUARED METER"
     }
 
     public fun rescan(view:View) {
